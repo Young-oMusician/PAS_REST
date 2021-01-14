07cce8bc94cb90b1c431981aa77c0db2d5657787
Paweł Bucki <mrpawrob@gmail.com>
2021-01-14 16:15:30 +0100

    ADDED authenticationController

---
+++ src/main/java/com/example/PAS_REST/restapp/auth/authenticateControler.java
@@ -0,0 +1,37 @@
+package com.example.PAS_REST.restapp.auth;
+
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+
+@Path("auth")
+public class authenticateControler {
+
+        private static final Logger LOGGER = Logger.getLogger(authenticateControler.class.getName());
+
+        @Inject
+        private SecurityContext securityContext;
+
+        @GET
+        @Path("login")
+        public Response login() {
+            LOGGER.log(Level.INFO, "login");
+            if (securityContext.getCallerPrincipal() != null) {
+                JsonObject result = Json.createObjectBuilder()
+                        .add("user", securityContext.getCallerPrincipal().getName())
+                        .build();
+                return Response.ok(result).build();
+            }
+            return Response.status(UNAUTHORIZED).build();
+        }
+
+
+}
